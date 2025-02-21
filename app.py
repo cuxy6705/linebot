6d901b738cfbd3a740163699a3795941f83c14af
王翊峰 <cuxy0000@gmail.com>
2025-02-21 15:24:09 +0800

    update app.py

--- app.py
+++ app.py
@@ -159,11 +159,15 @@ def cron():
         user_id = row['user_id']
         desc = row['text']
         notify_time = row['notify_time']  # ISO8601 字串
+        notify_time = datetime.fromisoformat(notify_time)
+        local_dt = utc_dt.astimezone(notify_time)
+        display_str = local_dt.strftime("%-m/%-d %H:%M")
+
         try:
             # 發送提醒
             line_bot_api.push_message(
                 user_id, 
-                TextSendMessage(text=f"提醒：{desc}\n(原設定時間: {notify_time})")
+                TextSendMessage(text=f"{display_str} {display_str})")
             )
 
             # 更新 is_sent 為 True
